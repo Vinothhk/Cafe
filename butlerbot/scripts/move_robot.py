8a7b75c59b4780d802927fb337963f5f671c855d
Vinothhk <iamvinothkumar16@gmail.com>
2024-07-06 11:35:33 +0530

    Comments Added

--- butlerbot/scripts/move_robot.py
+++ butlerbot/scripts/move_robot.py
@@ -11,22 +11,21 @@ from action_msgs.msg import GoalStatus
 class MoveRobot(Node):
     def __init__(self):
         super().__init__('MoveRobot')
-        self.action_client = ActionClient(self, NavigateToPose, 'navigate_to_pose')
+        self.action_client = ActionClient(self, NavigateToPose, 'navigate_to_pose')   #Create an Action Client
         self.current_goal_done = False
         self.process()
         
-    def process(self):
-        input_str = input('\n### USER ####\nDo you want to order anything (Y/N): ')
+    def process(self):                  #Interacts with USER (under ### USER ####) and Kitchen side (### KITCHEN ####)
+        input_str = input('\n### USER ####\nDo you want to order anything (Y/N): ')   
         if input_str=='Y' or input_str=='y':
-            order = []
-            canc = False    
+            order = []                  #To store the numbers of ordered tables  
+            canc = False                #BOOL VALUE. (TRUE - if any order get cancel or robot is not attended at the table)
             nos = input('\n### USER ####\nEnter the Table Numbers: ').split(' ')
             nos = list(map(int,nos))  
             nos.sort(); order = order+nos;
-            print(order)
             print(f'Order placed for Table {str(order)}')
             
-            self.move_to_kitchen()
+            self.move_to_kitchen()      #Moves to Kitchen once any order is placed
             
             now = time.time()
             future = now+10
@@ -38,10 +37,10 @@ class MoveRobot(Node):
                     self.move_to_home()
                     break
                              
-                conf = input('\n### KITCHEN ####\nPress y for confirmation: ')
+                conf = input('\n### KITCHEN ####\nPress y for confirmation: ')    #Asks Confirmation in Kitchen
                 now = time.time()
                 future= now+10
-                if now>future:
+                if now>future:                                                    #Time Out in Kitchen if not confirmed within 10 second & then robot moves to Home
                     print('Timed Out. Returning Home')
                     self.move_to_home()
                     break
@@ -50,8 +49,8 @@ class MoveRobot(Node):
                     print('Robot is moving to table')
                     c = input('\n### USER ####\nThe Order is Delivering...Would you like to cancel any of the order(Y/N) ? ')
                     if c == 'y' or c =='Y':
-                        canc = True   # BOOL VALUE. (TRUE - if any order get cancel or robot is not attended at the table)
-                        cn = input('\n### USER ####\nEnter the Table Numbers to cancel: ').split(' ')
+                        canc = True   
+                        cn = input('\n### USER ####\nEnter the Table Numbers to cancel: ').split(' ')           #Gets the table number for which the order has to cancelled
                         cn = list(map(int,cn)) 
                         for i,j in enumerate(cn):
                             order.remove(j)
@@ -88,17 +87,17 @@ class MoveRobot(Node):
                                 canc = True
                                 print('Skipping the table')
                      
-                    if canc == True:
+                    if canc == True:               #If any table didnt response to the order (or) any order cancelled
                         print('Moving to Kitchen and then Home')
                         self.move_to_kitchen()
                     self.move_to_home()
                     break
-                else:
+                else:                              #If not confirmed in the kitchen
                     print('Robot moving to home')
                     self.move_to_home()
                     break
                 
-    def confirm_order(self):
+    def confirm_order(self): #Asks Confirmation in table. Time out if response is not within 10 seconds
         now = time.time()
         future = now+10
         attend = int(input('\n### USER ####\nPls Press 1 to Confirm the Order: ')) 
@@ -108,31 +107,31 @@ class MoveRobot(Node):
             return False     
         else:
             if attend==1:
-                return True
+                return True            #Returns True When order is successfully attended at the table
             else:
                 return False
                 
-    def move_to_kitchen(self):
+    def move_to_kitchen(self):       #Sending goal pose of Kitchen coordinates
         self.get_logger().info('Moving to Kitchen')
         self.move_to_position(6.0,0.0,0.0)
     
-    def move_to_tabletwo(self):
+    def move_to_tabletwo(self):      #Sending goal pose of Table 2
         self.get_logger().info('Moving to table Two')
         self.move_to_position(-18.06,-3.13,-0.707)
             
-    def move_to_tablethree(self):
+    def move_to_tablethree(self):    #Sending goal pose of Table 3
         self.get_logger().info('Moving to table Three')
         self.move_to_position(-18.27,2.83,0.707)
         
-    def move_to_tableone(self):
+    def move_to_tableone(self):      #Sending goal pose of Table 1
         self.get_logger().info('Moving to table One')
         self.move_to_position(-9.84,-2.68,-0.707)
     
-    def move_to_home(self):
+    def move_to_home(self):          #Sending goal pose of Home Position
         self.get_logger().info('Moving to Home')
         self.move_to_position(-2.96,7.05,0.0)
 
-    def move_to_position(self, x, y, z):
+    def move_to_position(self, x, y, z):   #Sends a navigation goal to the action server, 
         self.current_goal_done = False
         goal_pose = NavigateToPose.Goal()
         goal_pose.pose.header.frame_id = 'map'
@@ -146,11 +145,11 @@ class MoveRobot(Node):
         while not self.current_goal_done:
             rclpy.spin_once(self)
             
-    def feedback_callback(self, feedback_msg):
+    def feedback_callback(self, feedback_msg): #process feedback from the Action Server
         feedback = feedback_msg.feedback
-        #self.get_logger().info('FEEDBACK:' + str(feedback) )
+        #self.get_logger().info('FEEDBACK:' + str(feedback))   
         
-    def goal_response_callback(self, future):
+    def goal_response_callback(self, future):  #Invoked when the action server responds to the goal request. Checks whether the goal was accepted or rejected.
         goal_handle = future.result()
         if not goal_handle.accepted:
             self.get_logger().info('Goal rejected')
@@ -161,7 +160,7 @@ class MoveRobot(Node):
         result_future = goal_handle.get_result_async()
         result_future.add_done_callback(self._wait_for_result)
 
-    def _wait_for_result(self, result_future):
+    def _wait_for_result(self, result_future):  #Called when the action server finishes processing the goal. retrieves and logs the result of the goal execution.
         result = result_future.result()
         if result.status == GoalStatus.STATUS_SUCCEEDED:
             self.get_logger().info('Goal succeeded!')
